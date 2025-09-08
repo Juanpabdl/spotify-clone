91beb659b1e9664366354899b7769b8867e94e39
JuanPabdl <a00825316@itesm.mx>
2025-09-08 17:49:57 -0600

    util: musicData file included & images imported

---
+++ src/utils/musicData.ts
@@ -0,0 +1,26 @@
+import top_global from '../assets/images/top50_global.png';
+import top_mex from '../assets/images/top50_mexico.png';
+
+export const albumData = [
+    {
+        id: 0,
+        name: 'Top 50 Global',
+        img: top_global,
+        desc:'Your weekly update of the most popular tracks'
+    },{
+        id: 1,
+        name: 'Top 50 Mexico',
+        img: top_mex,
+        desc:'Your weekly update of the most popular tracks'
+    },
+]
+
+export const songData = [
+    {
+        id:0,
+        name: 'Song One',
+        img: '',
+        file: '',
+        desc: 'Let´s turn that frown upside-down',
+    }
+]
