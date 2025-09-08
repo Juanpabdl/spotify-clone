8b63c82ad832ac618a4585ba03bc116e97e9386a
JuanPabdl <a00825316@itesm.mx>
2025-09-08 17:50:38 -0600

    feat: AlbumItem & SongItem components implementation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,35 @@
 import Navbar from "../../components/Navbar";
-
+import AlbumItem from "../../components/AlbumItem";
+import { albumData, songData } from "../../utils/musicData";
+import SongItem from "../../components/SongItem";
 const Home = () => {
     return(
         <div>
             <Navbar/>
+            <div className="mb-4">
+                <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+                <div className="flex overflow-auto no-scrollbar">
+                    {albumData.map((item,index) => (
+                        <AlbumItem key={`album-item-${index}`} 
+                        id={item.id}
+                        name={item.name}
+                        image={item.img}
+                        desc={item.desc}/>
+                    ))}
+                </div>
+            </div>
+            <div className="mb-4">
+                <h1 className="my-5 font-bold text-2xl">Today´s Biggest Hits</h1>
+                <div className="flex overflow-auto no-scrollbar">
+                    {songData.map((item,index) => (
+                        <SongItem key={`album-item-${index}`} 
+                        id={item.id}
+                        name={item.name}
+                        image={item.img}
+                        desc={item.desc}/>
+                    ))}
+                </div>
+            </div> 
         </div>
     )
 }
