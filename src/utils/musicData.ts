15c8fca2ef6179d4c3a56eb66a2b2a28b4f1b7d3
JuanPabdl <a00825316@itesm.mx>
2025-09-10 12:21:56 -0600

    chore: update musicData and images

--- src/utils/musicData.ts
+++ src/utils/musicData.ts
@@ -1,5 +1,10 @@
 import top_global from '../assets/images/top50_global.png';
 import top_mex from '../assets/images/top50_mexico.png';
+import song1 from '../assets/images/song1.jpeg';
+import song2 from '../assets/images/song2.jpeg';
+import song3 from '../assets/images/song3.jpeg';
+import song4 from '../assets/images/song4.jpeg';
+import song5 from '../assets/images/song5.jpeg';
 
 export const albumData = [
     {
@@ -19,8 +24,41 @@ export const songData = [
     {
         id:0,
         name: 'Song One',
-        img: '',
+        img: song1,
         file: '',
         desc: 'Let´s turn that frown upside-down',
-    }
+        duration: '3:00'
+    },
+    {
+        id:1,
+        name: 'Song Two',
+        img: song2,
+        file: '',
+        desc: 'Welcome to the machine',
+        duration: '3:00'
+    },
+    {
+        id:2,
+        name: 'Song Three',
+        img: song3,
+        file: '',
+        desc: 'Under the bridge downtown',
+        duration: '3:00'
+    },
+    {
+        id:3,
+        name: 'Song Four',
+        img: song4,
+        file: '',
+        desc: 'Dive into the dream',
+        duration: '3:00'
+    },
+    {
+        id:4,
+        name: 'Song Five',
+        img: song5,
+        file: '',
+        desc: 'Creatures of the night',
+        duration: '3:00'
+    },
 ]
