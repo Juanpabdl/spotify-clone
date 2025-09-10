bead756afa6a7d2ccc57228fda643a7f880aaedf
JuanPabdl <a00825316@itesm.mx>
2025-09-10 16:58:04 -0600

    feat (Layout): bgColor changes on Album page

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,13 +1,29 @@
+import { useEffect, useRef } from "react";
 import Player from "../../components/Player";
 import SideBar from "../../components/Sidebar";
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
+import { albumData } from "../../utils/musicData";
 //import ScrollToTop from "../../components/ScrollToTop";
 const Layout = () => {
+    const displayRef = useRef<HTMLDivElement>(null);
+    const location = useLocation();
+    const isAlbum = location.pathname.includes('album');
+    const albumId = isAlbum ? location.pathname.slice(-1) : '';
+    const bgColor = albumData[Number(albumId)].bgColor;
+
+    useEffect(() => {
+        if(isAlbum){
+            displayRef.current!.style.background = `linear-gradient(${bgColor}, var(--card))`;
+        }else{
+            displayRef.current!.style.background = `var(--card)`;
+        }
+    },[bgColor, isAlbum])
+
     return(
         <div className="h-screen bg-background">
             <div className="h-[90%] flex">
                 <SideBar/>
-                <div className="custom-scrollbar w-[100%] m-2 p-6 pt-4 bg-card text-foreground overflow-auto lg:w-[75%] lg:ml-0">
+                <div ref={displayRef} className="custom-scrollbar w-[100%] m-2 p-6 pt-4 bg-card text-foreground overflow-auto lg:w-[75%] lg:ml-0">
                     <Outlet/>
                 </div>
                 {/*<ScrollToTop/>*/}
