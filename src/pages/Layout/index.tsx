37728b21a09cf0c415a7b4898c754602e7b8ad17
JuanPabdl <a00825316@itesm.mx>
2025-09-11 16:34:39 -0600

    refact (Layout): layout color change logic passed down to display section

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,31 +1,13 @@
-import { useEffect, useRef } from "react";
 import Player from "../../components/Player";
 import SideBar from "../../components/Sidebar";
-import { Outlet, useLocation } from "react-router-dom"
-import { albumData } from "../../utils/data/musicData";
+import DisplaySection from "../../components/DisplaySection";
 
 const Layout = () => {
-    const displayRef = useRef<HTMLDivElement>(null);
-    const location = useLocation();
-    const isAlbum = location.pathname.includes('album');
-    const albumId = isAlbum ? location.pathname.slice(-1) : '';
-    const bgColor = albumData[Number(albumId)].bgColor;
-
-    useEffect(() => {
-        if(isAlbum){
-            displayRef.current!.style.background = `linear-gradient(${bgColor}, var(--card))`;
-        }else{
-            displayRef.current!.style.background = `var(--card)`;
-        }
-    },[bgColor, isAlbum])
-
     return(
         <div className="h-screen bg-background">
             <div className="h-[90%] flex">
                 <SideBar/>
-                <div ref={displayRef} className="custom-scrollbar w-[100%] m-2 p-6 pt-4 bg-card text-foreground overflow-auto lg:w-[75%] lg:ml-0">
-                    <Outlet/>
-                </div>
+                <DisplaySection/>
             </div>
             <Player/>
             <audio preload="auto"></audio>
