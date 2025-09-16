43e4dab5a8d52f382bbd18a456a58f3864e88302
JuanPabdl <a00825316@itesm.mx>
2025-09-16 16:40:04 -0600

    feat: defined playercontext & types

---
+++ src/utils/context/PlayerContext/index.ts
@@ -0,0 +1,39 @@
+import { createContext, type RefObject } from "react";
+
+type Time = {
+    currentTime: {
+        second: number,
+        minute: number,
+    },
+    totalTime: {
+        second: number,
+        minute: number
+    }
+}
+
+export type SongType = {
+    id: number,
+    name: string,
+    img: string,
+    file: string|null,
+    desc: string,
+    duration: string
+}
+
+export type PlayerContextType = {
+    isPlaying: boolean;
+    volume: number;
+    currentTrack: SongType;
+    audioRef: RefObject<HTMLAudioElement|null>;
+    barRef: RefObject<HTMLHRElement|null>,
+    time: Time,
+    play: () => void;
+    pause: () => void;
+    setIsPlaying: (playing: boolean) => void;
+    setCurrentTrack: (src: SongType) => void;
+    setVolume: (newVolume: number) => void;
+    setTime: (newTime: Time) => void;
+}
+
+export const PlayerContext = createContext<PlayerContextType|undefined>(undefined)
+
